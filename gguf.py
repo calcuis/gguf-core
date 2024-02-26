d50403d5f7442ff87c1b180d748b7399595a9761
calcuis <113646141+calcuis@users.noreply.github.com>
2024-02-26 10:19:12 +0800

    Update gguf.py

--- gguf.py
+++ gguf.py
@@ -1,6 +1,6 @@
 # !/usr/bin/env python3
 
-__version__="0.0.20"
+__version__="0.0.21"
 
 import argparse, json, os.path, urllib.request
 from tkinter import *
@@ -13,15 +13,12 @@ def pdf_handler(llm):
         # Remove newline characters and join lines into one
         joined_text = ' '.join(input_text.splitlines())
         return joined_text
-
+    
     if pdf_files:
         print("PDF file(s) available. Select which one to use:")
-        
         for index, file_name in enumerate(pdf_files, start=1):
             print(f"{index}. {file_name}")
-
         choice = input(f"Enter your choice (1 to {len(pdf_files)}): ")
-        
         try:
             choice_index=int(choice)-1
             selected_file=pdf_files[choice_index]
@@ -31,33 +28,39 @@ def pdf_handler(llm):
             # from llama_core.rpdf import PdfReader # generic module adopted
             from llama_core.pdf import PdfReader # lama_core 0.1.1 rpdf => pdf
             reader = PdfReader(selected_file)
-
             text=""
             number_of_pages = len(reader.pages)
             for i in range(number_of_pages):
                 page = reader.pages[i]
                 text += page.extract_text()
             # # print(text)
-            
+                
             # Join text
             output_text = join_text(text)
             inject = f"analyze the content below: "+output_text
 
             print(f"\nPDF cotent extracted as below:\n\n"+text)
             input("---Enter to analyze the PDF content above---")
-
-            print("Processing...")
-            # output = llm("Q: "+inject, max_tokens=4096, echo=True)
-            output = llm("Q: "+inject, max_tokens=32768, echo=True)
-            answer = output['choices'][0]['text']
-            print(answer+"\n")
+            
+            # print("Processing...")
+            # ###########################################
+            # # # output = llm("Q: "+inject, max_tokens=4096, echo=True)
+            # ###########################################
+            from llama_core.rich.progress import Progress
+            with Progress(transient=True) as progress:
+                task = progress.add_task("Processing", total=None)
+                output = llm("Q: "+inject, max_tokens=32768, echo=True)
+                answer = output['choices'][0]['text']
+                print(answer+"\n")
+            # ###########################################
 
         except (ValueError, IndexError):
             print("Invalid choice. Please enter a valid number.")
     else:
         print("No PDF files are available in the current directory.")
         input("--- Press ENTER To Exit ---")
-        
+# ###########################################################################
+# from rich.progress import Progress (rich dropped)
 from llama_core.rich.progress import Progress # generic module adopted (lama_core 0.1.2)
 
 def get_file_size(url):
@@ -72,7 +75,7 @@ def clone_file(url):
     try:
         file_size = get_file_size(url)
         filename = os.path.basename(url)
-        with Progress() as progress:
+        with Progress(transient=True) as progress:
             task = progress.add_task(f"Downloading {filename}", total=file_size)
             with urllib.request.urlopen(url) as response, open(filename, 'wb') as file:
                 chunk_size = 1024
@@ -84,9 +87,10 @@ def clone_file(url):
                     file.write(chunk)
                     downloaded += len(chunk)
                     progress.update(task, completed=downloaded, description=f"Downloading {filename} [green][{format_size(downloaded)} / {format_size(file_size)}]")
-        print(f"File cloned successfully and saved as '{filename}' in the current directory.")
+        print(f"File cloned successfully and saved as '{filename}'({format_size(file_size)}) in the current directory.")
     except Exception as e:
         print(f"Error: {e}")
+# ###########################################################################
 
 def read_json_file(file_path):
     response = urllib.request.urlopen(file_path)
@@ -149,41 +153,78 @@ def __init__():
     elif args.subcommand == 'p':
         import os
         gguf_files = [file for file in os.listdir() if file.endswith('.gguf')]
-
         if gguf_files:
             print("GGUF file(s) available. Select which one to use:")
-            
             for index, file_name in enumerate(gguf_files, start=1):
                 print(f"{index}. {file_name}")
-
             choice = input(f"Enter your choice (1 to {len(gguf_files)}): ")
-            
             try:
                 choice_index=int(choice)-1
                 selected_file=gguf_files[choice_index]
                 print(f"Model file: {selected_file} is selected!")
                 ModelPath=selected_file
-
                 print("Processing...")
-
                 from llama_core import Llama
                 llm = Llama(model_path=ModelPath)
-
                 while True:
                     ask = input("---Enter to select a PDF file (Q for quit)---")
-                    if ask.lower() == "q":
+                    # if ask == "q" or ask == "Q":
+                    if ask.lower() == 'q':
                         break
                     pdf_handler(llm)
-
             except (ValueError, IndexError):
                 print("Invalid choice. Please enter a valid number.")
         else:
             print("No GGUF files are available in the current directory.")
             input("--- Press ENTER To Exit ---")
-
         print("Goodbye!")
 
-    elif args.subcommand == 'c':
-        from llama_core import cli
     elif args.subcommand == 'g':
         from llama_core import gui
+    # elif args.subcommand == 'c':
+    #     from llama_core import cli
+    elif args.subcommand == 'c':
+        import os
+        def clear():
+            # for windows
+            if os.name == 'nt':
+                _ = os.system('cls')
+            # for mac and linux(here, os.name is 'posix')
+            else:
+                _ = os.system('clear')
+        gguf_files = [file for file in os.listdir() if file.endswith('.gguf')]
+        if gguf_files:
+            print("GGUF file(s) available. Select which one to use:")
+            for index, file_name in enumerate(gguf_files, start=1):
+                print(f"{index}. {file_name}")
+            choice = input(f"Enter your choice (1 to {len(gguf_files)}): ") 
+            try:
+                choice_index=int(choice)-1
+                selected_file=gguf_files[choice_index]
+                print(f"Model file: {selected_file} is selected!")
+                ModelPath=selected_file
+                from llama_core import Llama
+                llm = Llama(model_path=ModelPath)
+                # ##########################################################
+                clear()
+                while True:
+                    ask = input("Enter a Question (Q for quit): ")
+                    if ask.lower() == 'q':
+                        break
+                    clear()
+                    from llama_core.rich.progress import Progress
+                    with Progress(transient=True) as progress:
+                        task = progress.add_task("Processing", total=None)
+                        output = llm("Q: "+ask, max_tokens=2048, echo=True)
+                        answer = output['choices'][0]['text']
+                        token_info = output["usage"]["total_tokens"]
+                        clear()
+                        print("Raw input: "+ask+" (token used: "+str(token_info)+")\n")
+                        print(answer+"\n")
+                # ##########################################################
+            except (ValueError, IndexError):
+                print("Invalid choice. Please enter a valid number.")
+        else:
+            print("No GGUF files are available in the current directory.")
+            input("--- Press ENTER To Exit ---")
+        print("Goodbye!")
