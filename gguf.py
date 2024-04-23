40dc86aa85b3c97650f0a5222a8596ad323a6274
calcuis <113646141+calcuis@users.noreply.github.com>
2024-04-24 07:15:32 +0800

    Update gguf.py

--- gguf.py
+++ gguf.py
@@ -1,9 +1,46 @@
 # !/usr/bin/env python3
 
-__version__="0.0.35"
+__version__="0.0.36"
 
 import argparse, json, os.path, urllib.request
-
+def wav_handler_online(llm):
+    import os
+    wav_files = [file for file in os.listdir() if file.endswith('.wav')]
+    if wav_files:
+        print("WAV file(s) available. Select which one to use:")
+        for index, file_name in enumerate(wav_files, start=1):
+            print(f"{index}. {file_name}")
+        choice = input(f"Enter your choice (1 to {len(wav_files)}): ")
+        try:
+            choice_index=int(choice)-1
+            selected_file=wav_files[choice_index]
+            print(f"WAV file: {selected_file} is selected!")
+            import speech_recognition as sr
+            r = sr.Recognizer()
+            with sr.AudioFile(selected_file) as source:
+                audio = r.record(source)
+            try:
+                text = r.recognize_google(audio)
+                # text = r.recognize_sphinx(audio)
+                from llama_core.rich.console import Console
+                console = Console()
+                console.print(f"\n[green]Speech/voice content recognized as: [yellow]"+text)
+                from llama_core.rich.progress import Progress
+                with Progress(transient=True) as progress:
+                    task = progress.add_task("Processing", total=None)
+                    output = llm("\nQ: "+text, max_tokens=16384, echo=True)
+                    answer = output['choices'][0]['text']
+                    print(answer+"\n")
+            except sr.UnknownValueError:
+                print("Could not understand audio content")
+            except sr.RequestError as e:
+                print("Error; {0}".format(e))
+        except (ValueError, IndexError):
+            print("Invalid choice. Please enter a valid number.")
+    else:
+        print("No WAV files are available in the current directory.")
+        input("--- Press ENTER To Exit ---")
+# ###########################################################################
 def pdf_handler(llm):
     import os
     pdf_files = [file for file in os.listdir() if file.endswith('.pdf')]
